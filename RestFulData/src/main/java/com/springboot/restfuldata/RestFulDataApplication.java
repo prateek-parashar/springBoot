7efe59416e13a2152a69be0b1bda8bb44e335f72
prateek-parashar <36658243+prateek-parashar@users.noreply.github.com>
2022-10-08 19:16:44 +0530

    Added the project for spring data template

---
+++ RestFulData/src/main/java/com/springboot/restfuldata/RestFulDataApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.restfuldata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestFulDataApplication.class, args);
+    }
+
+}
---
+++ RestFulData/src/test/java/com/springboot/restfuldata/RestFulDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.restfuldata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFulDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
