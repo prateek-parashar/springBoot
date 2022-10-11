3d8f45d82db272c79d7dea3eba596f4c1bd4fbf3
prateek-parashar <36658243+prateek-parashar@users.noreply.github.com>
2022-10-11 15:46:02 +0530

    Add the planefinder project to the repo

---
+++ PlaneFinder/src/test/java/com/springnboot/PlaneFinder/PlaneFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springnboot.PlaneFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
