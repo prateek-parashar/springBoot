17a69449210cc4b8375a0f72b8d182a92db958a9
prateek-parashar <36658243+prateek-parashar@users.noreply.github.com>
2022-10-18 14:18:26 +0530

    Adding reactive h2 for repository access

--- RestFulAPI/src/main/java/com/springboot/restfulapi/dao/AircraftRepository.java
+++ RestFulAPI/src/main/java/com/springboot/restfulapi/dao/AircraftRepository.java
@@ -1,9 +1,9 @@
 package com.springboot.restfulapi.dao;
 
 import com.springboot.restfulapi.models.Aircraft;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AircraftRepository extends JpaRepository<Aircraft, Long> {
+public interface AircraftRepository extends ReactiveCrudRepository<Aircraft, Long> {
 }
